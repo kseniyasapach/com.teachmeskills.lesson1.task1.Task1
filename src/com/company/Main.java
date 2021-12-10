2af7b5a9262adaa4712abc238f014b4e72430111
Ксения Сапач <K.Sapach@NovaCom.by>
2021-12-10 09:43:16 +0300

    first-task

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+
+
+
+    public static void main(String[] args) {
+
+        final double newDouble = 3.14;
+        int a = 10, b = 5, r = 8;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        // write your code here
+    }
+}
